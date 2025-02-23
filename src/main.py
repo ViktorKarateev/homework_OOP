6e6d9928d77b9cf0bb7b92e5825bdf59eeb9924f
Karateev <karateev.92@mail.ru>
2025-02-23 11:39:50 +0400

    Вставленна из задания к уроку

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
-import os
 from models import Product, Category
 from utils import load_categories_from_json
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(str(product1))
+    print(str(product2))
+    print(str(product3))
 
     category1 = Category(
         "Смартфоны",
@@ -14,46 +17,22 @@ if __name__ == "__main__":
         [product1, product2, product3]
     )
 
-    print(category1.products)  # Проверка работы геттера
+    print(str(category1))
 
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products)  # Проверка добавления продукта
-    print(category1.product_count)  # Проверка обновления счетчика продуктов
+    print(category1.products)
 
-    products_list = [product1, product2, product3, product4]  # Создаем список товаров
-
-    new_product = Product.new_product(
-        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-         "quantity": 5},
-        products_list  # Передаем список товаров
-    )
-
-    print(new_product.name)
-    print(new_product.description)
-    print(new_product.price)
-    print(new_product.quantity)
-
-    new_product.price = 800  # Проверка сеттера (новая цена)
-    print(new_product.price)
-
-    new_product.price = -100  # Проверка защиты от отрицательной цены
-    print(new_product.price)
-
-    new_product.price = 0  # Проверка защиты от нуля
-    print(new_product.price)
+    print(product1 + product2)
+    print(product1 + product3)
+    print(product2 + product3)
 
 
-# Автоматически находим путь к JSON-файлу
-file_path = os.path.join(os.path.dirname(__file__), "../data/products.json")
-
-# Загружаем данные
-categories = load_categories_from_json(file_path)
-
+# Загружаем данные из products.json
+categories = load_categories_from_json("products.json")
 
 # Выводим загруженные категории
 for category in categories:
     print(f"\nКатегория: {category.name}")
     print(f"Описание: {category.description}")
     print(f"Количество товаров: {len(category.products)}")
-    print(f"Товары: {category.products}")
+    print(f"Товары:\n{category.products}")
+
